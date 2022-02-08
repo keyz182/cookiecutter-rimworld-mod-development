472f95d6229756e5eafd24df9fafaf4470da1a27
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-02-08 13:22:21 -0800

    Better settings code

--- {{cookiecutter.mod_name}}/{{cookiecutter.mod_name}}/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.cs
+++ {{cookiecutter.mod_name}}/{{cookiecutter.mod_name}}/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.cs
@@ -7,14 +7,15 @@ using UnityEngine;
 {%endif%}
 namespace {{cookiecutter.mod_name.replace(' ', '_')}}
 {
-	public class Mod : Verse.Mod
+{%if(cookiecutter.settings != 'n')%}	public static Settings settings;
+{%endif%}	public class Mod : Verse.Mod
 	{
 		public Mod(ModContentPack content) : base(content)
 		{
 {%if(cookiecutter.helloWorld != 'n')%}			Log.Message("Hello world from {{cookiecutter.mod_name}}");
 
 {% endif %}{%if(cookiecutter.settings != 'n')%}			// initialize settings
-			GetSettings<Settings>();
+			settings = GetSettings<Settings>();
 
 {%endif%}{%if(cookiecutter.harmony != 'n')%}#if DEBUG
 			Harmony.DEBUG = true;
@@ -27,7 +28,7 @@ namespace {{cookiecutter.mod_name.replace(' ', '_')}}
 		public override void DoSettingsWindowContents(Rect inRect)
 		{
 			base.DoSettingsWindowContents(inRect);
-			GetSettings<Settings>().DoWindowContents(inRect);
+			settings.DoWindowContents(inRect);
 		}
 
 		public override string SettingsCategory()
