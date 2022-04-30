752306093308814c91c20ecc257d580bd1049171
Dexter Lowe <feldoh10@gmail.com>
2022-04-30 22:20:11 +0100

    Fix path issues

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,8 @@
 import os
 
 #About is always made
-os.makedirs("{{cookiecutter.mod_name}}/{{cookiecutter.mod_ver}}/Defs", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/{{cookiecutter.mod_ver}}/Patches", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Common/Languages", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Common/Sounds", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Common/Textures", exist_ok=True)
+os.makedirs("{{cookiecutter.mod_ver}}/Defs", exist_ok=True)
+os.makedirs("{{cookiecutter.mod_ver}}/Patches", exist_ok=True)
+os.makedirs("Common/Languages", exist_ok=True)
+os.makedirs("Common/Sounds", exist_ok=True)
+os.makedirs("Common/Textures", exist_ok=True)
