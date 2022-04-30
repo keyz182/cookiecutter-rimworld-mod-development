51980b5ca193b19e800047e01e20396931130389
Dexter Lowe <feldoh10@gmail.com>
2022-04-30 22:06:44 +0100

    Versioned folders

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,8 @@
 import os
 
 #About is always made
-os.makedirs("{{cookiecutter.mod_name}}/Defs", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Patches", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Languages", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Sounds", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Textures", exist_ok=True)
+os.makedirs("{{cookiecutter.mod_name}}/{{cookiecutter.mod_ver}}/Defs", exist_ok=True)
+os.makedirs("{{cookiecutter.mod_name}}/{{cookiecutter.mod_ver}}/Patches", exist_ok=True)
+os.makedirs("{{cookiecutter.mod_name}}/Common/Languages", exist_ok=True)
+os.makedirs("{{cookiecutter.mod_name}}/Common/Sounds", exist_ok=True)
+os.makedirs("{{cookiecutter.mod_name}}/Common/Textures", exist_ok=True)
