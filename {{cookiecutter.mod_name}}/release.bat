4805c078a7380c5888ed4ebd0552fb73fd512ca4
Dexter Lowe <feldoh10@gmail.com>
2024-05-27 19:52:34 +0100

    Fix Debuglog

--- {{cookiecutter.mod_name}}/release.bat
+++ {{cookiecutter.mod_name}}/release.bat
@@ -1,5 +1,14 @@
 @ECHO OFF
 ECHO Building {{cookiecutter.mod_name}} {{cookiecutter.mod_ver}} - Started
+
+dotnet --list-sdks | findstr /R /C:"8\..*" 1>nul
+if %errorlevel% neq 0 (
+    ECHO .NET 8 SDK not found. Please install it using the following command:
+    ECHO winget install Microsoft.DotNet.SDK.8
+    PAUSE > NUL
+    EXIT /B
+)
+
 @ECHO ON
 dotnet restore {{cookiecutter.mod_ver}}/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.sln
 dotnet build {{cookiecutter.mod_ver}}/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.sln /p:Configuration=Release
