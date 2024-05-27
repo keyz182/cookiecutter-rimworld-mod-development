c5b93dc037fc863f4af0481eebc97d83815aa148
Dexter Lowe <feldoh10@gmail.com>
2024-05-27 19:39:08 +0100

    Release issue

--- {{cookiecutter.mod_name}}/release.bat
+++ {{cookiecutter.mod_name}}/release.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 ECHO Building {{cookiecutter.mod_name}} {{cookiecutter.mod_ver}} - Started
 @ECHO ON
-dotnet restore {{cookiecutter.mod_name}}/{{cookiecutter.mod_ver}}/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.sln
-dotnet build {{cookiecutter.mod_name}}/{{cookiecutter.mod_ver}}/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.sln /p:Configuration=Release
+dotnet restore {{cookiecutter.mod_ver}}/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.sln
+dotnet build {{cookiecutter.mod_ver}}/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.sln /p:Configuration=Release
 @ECHO OFF
 ECHO Building {{cookiecutter.mod_name}} {{cookiecutter.mod_ver}} - Complete
 ECHO Press any key to exit...
